b3e75586757fde25a688e600d1ada89b35f9d2cd
xander <karimi.xander@gmail.com>
2023-03-26 13:45:28 +0330

    change codes

--- src/components/students/students.js
+++ src/components/students/students.js
@@ -6,32 +6,7 @@ const Students = (props) => {
   useEffect(() => {
     console.log("changed");
   }, [props.studentsList]);
-  if (props.toggle) {
-    return (
-      <div className="student-section">
-        {props.studentsList.map((student, index) => {
-          return (
-            <Student
-              key={index}
-              id={student.id}
-              name={student.name}
-              phone={student.phone}
-              email={student.email}
-              classNumber={student.classNumber}
-              nameChange={(event) => props.nameChange(event, student.id)}
-              changeEmail={(event) => props.changeEmail(event, student.id)}
-              changePhone={(event) => props.changePhone(event, student.id)}
-              changeClassNumber={(event) =>
-                props.changeClassNumber(event, student.id)
-              }
-              delete={props.delete}
-            />
-          );
-        })}
-      </div>
-    );
-  }
-  return props.studentsList.map((student, index) => {
+  let studentsList = props.studentsList.map((student, index) => {
     return (
       <Student
         key={index}
@@ -49,6 +24,12 @@ const Students = (props) => {
         delete={props.delete}
       />
     );
+  });
+  if (props.toggle) {
+    return <div className="student-section">{studentsList}</div>;
+  }
+  return props.studentsList.map((student, index) => {
+    return studentsList;
   });
 };
 export default Students;
