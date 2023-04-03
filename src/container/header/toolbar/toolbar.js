454c88461e0691dbad256265af66a1520faa2743
xander <karimi.xander@gmail.com>
2023-04-03 16:04:44 +0330

    add logo

--- src/container/header/toolbar/toolbar.js
+++ src/container/header/toolbar/toolbar.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./toolbar.css";
+import logo from "../../../components/logo/logo";
+import Logo from "../../../components/logo/logo";
 const Toolbar = (props) => {
   return (
     <header className="toolbar">
-      <div>logo</div>
+      <Logo />
       <nav>nav item</nav>
       <button>ورود</button>
     </header>
