477ed61d417cf6733a852e970993f224ea94963a
xander <karimi.xander@gmail.com>
2023-04-04 15:54:34 +0330

    add menu bar

--- src/container/header/toolbar/toolbar.js
+++ src/container/header/toolbar/toolbar.js
@@ -2,11 +2,14 @@ import React from "react";
 import "./toolbar.css";
 import logo from "../../../components/logo/logo";
 import Logo from "../../../components/logo/logo";
+import MeniItems from "../menu_item/menuItems";
 const Toolbar = (props) => {
   return (
     <header className="toolbar">
       <Logo />
-      <nav>nav item</nav>
+      <nav>
+        <MeniItems />
+      </nav>
       <button>ورود</button>
     </header>
   );
